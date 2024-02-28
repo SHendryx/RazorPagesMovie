b2d91f7f76339e5e5d8c73adb854a47f2336bec7
Samuel Hendryx <samuelhendryx@outlook.com>
2024-02-28 13:07:55 -0800

    Add seeddata. Modify pages.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,12 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models;
 public class Movie
 {
+    // Id
     public int Id { get; set; }
+    
+    // Title
     public string? Title { get; set; }
+
+    // Release Date
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set;}
+    
+    // Genre
     public string? Genre { get; set; }
+
+    // Price
+    [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 }
